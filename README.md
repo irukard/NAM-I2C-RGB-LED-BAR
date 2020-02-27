3eca0f5a931048dcea6fea47328bc9b434305be4
Krzysztof Daszuta <irukard@gmail.com>
2020-02-27 19:33:50 +0100

    first tests
    Doesn't work... :(

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# NAM-I2C-RGB-LED-BAR
-Nettigo Air Monitor I2C RGB LED BAR
+# Nettigo Air Monitor I2C RGB LED Bar
+
+Firmware for I2C Slave RGB LED BAR based on Neopixel LEDs.
+
+## Hardware
+This firmware is written with [Nettigo Air Monitor 0.3 I2C RGB LED Bar](https://easyeda.com/nettigo/Nettigo-Air-Monitor-I2C-RGB-LED-Bar) in mind.
+
+Device is based on ATtiny84A with ten WS2812E Neopixel LEDs. It meant to be a slave I2C device driving Neopixels acordingly to recived commands from I2C master. To make it 3V3 compatible board itself doesn't have I2C pullups (and since I2C is open drain it doesn't change logic level during communication). ATtiny84A is powered from 5V rail to ensure propper logic level in communication with LEDs.
+
+### Wiring
+
+- I2C_SDA - PIN4 - PA6 (PCINT6/OC1A/SDA/MOSI/DI/ADC6)
+- I2C_SCL - PIN6 - PA4 (PCINT4/T1/SCL/USCK/ADC4)
+- WS2812E - PIN10 - PA0 (PCINT1/AIO0/ADC1)
---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+#include <Arduino.h>
+#include <NeoPixelBus.h>
+
+const uint16_t PixelCount = 10;
+const uint8_t PixelPin = 10;
+
+#define colorSaturation 128
+
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
+//NeoPixelBus<NeoRgbFeature, Neo400KbpsMethod> strip(PixelCount, PixelPin);
+
+RgbColor red(colorSaturation, 0, 0);
+RgbColor green(0, colorSaturation, 0);
+RgbColor blue(0, 0, colorSaturation);
+RgbColor white(colorSaturation);
+RgbColor black(0);
+
+void setup()
+{
+    strip.Begin();
+    strip.Show();
+}
+
+
+void loop()
+{
+  
+    // set the colors, 
+    // if they don't match in order, you need to use NeoGrbFeature feature
+    strip.SetPixelColor(0, red);
+    strip.SetPixelColor(1, green);
+    strip.SetPixelColor(2, blue);
+    strip.SetPixelColor(3, white);
+    // the following line demonstrates rgbw color support
+    // if the NeoPixels are rgbw types the following line will compile
+    // if the NeoPixels are anything else, the following line will give an error
+    //strip.SetPixelColor(3, RgbwColor(colorSaturation));
+    strip.Show();
+
+
+    delay(5000);
+
+    // turn off the pixels
+    strip.SetPixelColor(0, black);
+    strip.SetPixelColor(1, black);
+    strip.SetPixelColor(2, black);
+    strip.SetPixelColor(3, black);
+    strip.Show();
+
+    delay(5000);
+}
